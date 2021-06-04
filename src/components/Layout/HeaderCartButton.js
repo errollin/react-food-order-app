1604d244609f752592838176dfc68db516172719
ErrolLin <errolriccolin@foxmail.com>
2021-06-05 00:18:38 +0800

    achieve the function of closing the modal box

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,20 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import classes from './HeaderCartButton.module.css';
-import CartIcon from '../Cart/CartIcon';
+import classes from "./HeaderCartButton.module.css";
+import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = (props) => {
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>
-        3
-      </span>
+      <span className={classes.badge}>3</span>
     </button>
-  )
-}
+  );
+};
 
 export default HeaderCartButton;
